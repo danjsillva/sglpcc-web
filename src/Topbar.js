15acf2e3feed0b5a685161cc05f28966374e8217
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-03-15 16:50:02 -0300

    Adicionado link na topbar para pagina de licitacoes

--- src/Topbar.js
+++ src/Topbar.js
@@ -13,11 +13,13 @@ export default class Topbar extends Component {
               </span>
             </span> */}
             <span className="navbar-item">
-              {/* <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28" /> */}
-              <span className="title is-6 has-text-white">Sistema de Gestão em Licitações Públicas
+              <NavLink to="/licitacoes">
+                {/* <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28" /> */}
+                <span className="title is-6 has-text-white">Sistema de Gestão em Licitações Públicas
                 <br />
-                <span className="subtitle is-7 has-text-white">com foco no Combate à Corrupção</span>
-              </span>
+                  <span className="subtitle is-7 has-text-white">com foco no Combate à Corrupção</span>
+                </span>
+              </NavLink>
             </span>
 
             <span role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
