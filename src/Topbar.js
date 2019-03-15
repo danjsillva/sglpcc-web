df035c70c6389045624e81d96e89c1edfaf9ed28
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-03-15 20:44:22 -0300

    Correcoes topbar e layout

--- src/Topbar.js
+++ src/Topbar.js
@@ -7,18 +7,17 @@ export default class Topbar extends Component {
       <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
         <div className="container">
           <div className="navbar-brand">
-            {/* <span className="navbar-item">
+            <span className="navbar-item">
               <span className="title has-text-white is-3">
-                <i className="fa fa-search-dollar"></i>
+                <i className="fa fa-chart-pie has-text-grey-light"></i>
+                {/* <i className="fa fa-search-dollar"></i> */}
               </span>
-            </span> */}
+            </span>
             <span className="navbar-item">
               <NavLink to="/licitacoes">
                 {/* <img src="https://bulma.io/images/bulma-logo.png" alt="logo" width="112" height="28" /> */}
-                <span className="title is-6 has-text-white">Sistema de Gestão em Licitações Públicas
-                <br />
-                  <span className="subtitle is-7 has-text-white">com foco no Combate à Corrupção</span>
-                </span>
+                <div className="title is-6 has-text-white mb-0">Sistema de Gestão em Licitações Públicas</div>
+                <span className="subtitle is-7 has-text-grey-light">com foco no Combate à Corrupção</span>
               </NavLink>
             </span>
 
