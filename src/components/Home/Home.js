0dd3c8719b9302d0cf62aa3296f8d845632c3464
Daniel Silva <danielsilva@Daniels-MacBook-Air.local>
2019-03-27 10:02:23 -0300

    Corrigido footer e adicionado imagem na home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import GitHubButton from 'react-github-btn'
 
 import ImagePageLicitacoes from '../../assets/page-licitacoes.png'
+import ImagePageItens from '../../assets/page-itens.png'
 import ImagePageMateriais from '../../assets/page-materiais.png'
 
 export default function Home() {
@@ -87,41 +88,65 @@ export default function Home() {
         <div className="container pt-50">
           <div className="columns">
             <div className="column is-4 has-text-right">
-              <h1 className="title is-4 mt-100">Análise dos preços praticados nos itens da licitação</h1>
-              <h3 className="subtitle">Compare o preço de cada item com a média praticada nos processos de compra. Filtre por unidade, estado ou fornecedor para refinar os resultados.</h3>
+              <h1 className="title is-4 mt-100">Busca de licitações para análise de processos semelhantes</h1>
+              <h3 className="subtitle">Compare uma licitação com outros processos de compra. Filtre por unidade, estado ou fornecedor para refinar os resultados.</h3>
             </div>
             <div className="column is-8">
               <img src={ImagePageLicitacoes} alt="Página de licitações" className="custom-home-image pb-0" />
             </div>
           </div>
         </div>
+      </div>
 
-        <div className="container mt-50">
+      <div className="has-background-dark pb-50">
+        <div className="container pt-50">
           <div className="columns">
             <div className="column is-8">
-              <img src={ImagePageMateriais} alt="Página de licitações" className="custom-home-image pb-0" />
+              <img src={ImagePageItens} alt="Página de licitações" className="custom-home-image pb-0" />
             </div>
             <div className="column is-4">
+              <h1 className="title is-4 mt-100 has-text-white">Análise dos preços praticados nos itens da licitação</h1>
+              <h3 className="subtitle has-text-light">Compare o preço de cada item com a média praticada nos processos de compra. Filtre por unidade, estado ou fornecedor para refinar os resultados.</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="has-background-light pb-50">
+        <div className="container pt-50">
+          <div className="columns">
+            <div className="column is-4 has-text-right">
               <h1 className="title is-4 mt-100">Análise de preços do material nos processos de compra</h1>
               <h3 className="subtitle">Menor preço, maior preço, gráficos entre outros detalhes. Filtre por unidade, estado ou fornecedor para refinar os resultados.</h3>
             </div>
+            <div className="column is-8">
+              <img src={ImagePageMateriais} alt="Página de licitações" className="custom-home-image pb-0" />
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="has-background-dark m-0">
-        <div className="container pt-25 pb-25">
-          <div className="columns">
-            <div className="column is-8">
-              <h1 className="title is-5 has-text-white">Sistema de Gestão em Licitações Públicas</h1>
-              <h2 className="subtitle is-5 has-text-white">Com foco no combate à corrupção</h2>
-            </div>
-            <div className="column is-4">
-              <h1 className="title has-text-white"></h1>
+      <footer>
+        <div className="has-background-link m-0">
+          <div className="container pt-25 pb-25">
+            <div className="columns">
+              <div className="column is-8">
+                <h1 className="title is-5 has-text-white">Sistema de Gestão em Licitações Públicas</h1>
+                <h2 className="subtitle is-6 has-text-white">Com foco no combate à corrupção</h2>
+              </div>
+              <div className="column is-4 has-text-right" style={{ marginTop: 'auto' }}>
+                <Link to="/sobre" className="button is-link is-inverted is-outline is-rounded is-mediu">Saiba mais sobre o projeto</Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+
+        <div className="has-background-dark m-0">
+          <div className="container pt-5 pb-5 has-text-grey">
+            <small>Developed with <i className="fa fa-heart"></i> by <a href="http://instagram.com.br/danjsillva" target="blank" className="link has-text-grey">Daniel Silva</a></small>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
